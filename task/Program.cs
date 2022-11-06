08b98601229a2df6aeb1f2132a21a7072dc053ef
Andrew <it.andr@outlook.com>
2022-11-06 17:00:24 +0300

    We create and print a array

---
+++ task/Program.cs
@@ -0,0 +1,19 @@
+﻿int size = 6;
+string[] array = new string[size];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine();
+    Console.WriteLine("[" + String.Join(" ,", array) + "]");
+}
